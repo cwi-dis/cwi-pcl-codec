68449aac7f1ddd6d0a026417cec50beb59bb9def
Kees Blom <kees@cwi.nl>
2017-08-28 15:03:09 +0200

    Fixed a bug in #include files that have been moved from 'pcl/io/impl'  in the original source tree to '/pcl/compression/impl'  in the pcl-1.8 installer tree

--- cloud_codec_v2/include/pcl/cloud_codec_v2/impl/point_cloud_codec_v2_impl.hpp
+++ cloud_codec_v2/include/pcl/cloud_codec_v2/impl/point_cloud_codec_v2_impl.hpp
@@ -41,10 +41,10 @@
 #include <Eigen/StdVector>
 
 // point cloud compression from PCL
-#include <pcl/io/impl/entropy_range_coder.hpp>
+#include <pcl/compression/impl/entropy_range_coder.hpp>
 #include <pcl/cloud_codec_v2/point_cloud_codec_v2.h>
 #include <pcl/compression/point_coding.h>
-#include <pcl/io/impl/octree_pointcloud_compression.hpp>
+#include <pcl/compression/impl/octree_pointcloud_compression.hpp>
 #include <pcl/filters/radius_outlier_removal.h>
 #if defined(_OPENMP)
 #include <omp.h>
