ee30ad68ab478d0fa22c84d629676bfba85e4602
Jack Jansen <Jack.Jansen@cwi.nl>
2019-02-15 15:20:24 +0100

    Get rid of ply_reader workaround, it seems unneeded currently.

--- apps/cwipc_encode/cwipc_encode.cpp
+++ apps/cwipc_encode/cwipc_encode.cpp
@@ -18,20 +18,10 @@ int main(int argc, char** argv)
     //
     pcl::PointCloud<pcl::PointXYZRGB> *pc = new pcl::PointCloud<pcl::PointXYZRGB>;
     pcl::PLYReader ply_reader;
-#ifdef WITH_PLY_READER_BUG
-	pcl::PolygonMesh mesh;
-    if (ply_reader.read(argv[1], mesh) < 0) {
-        std::cerr << argv[0] << ": Error reading mesh from " << argv[1] << std::endl;
-        return 1;
-    }
-    pcl::fromPCLPointCloud2(mesh.cloud, *pc);
-#else
     if (ply_reader.read(argv[1], *pc) < 0) {
         std::cerr << argv[0] << ": Error reading pointcloud from " << argv[1] << std::endl;
         return 1;
     }
-
-#endif
     std::cerr << "Read pointcloud successfully, " << pc->size() << " points." << std::endl;
     //
     // Compress
