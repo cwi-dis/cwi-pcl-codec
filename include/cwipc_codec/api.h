fd11158fc7814dbedfa5e3b4abd04edede6391d4
JANSEN Jack <jack.jansen@cwi.nl>
2019-04-02 00:44:05 +0200

    On Linux we have to add /usr/local/lib to the search path to make find_library work (even though loading libraries works just fine)

--- include/cwipc_codec/api.h
+++ include/cwipc_codec/api.h
@@ -26,22 +26,33 @@
 #define CSHARP_COMPAT_ALLOC(nbytes) malloc(nbytes)
 #endif
 
-#ifdef __cplusplus
-//
-// Parameters to give to the encoder
-//
+/** \brief Pointcloud encoder settings.
+ *
+ * Various parameters governing encoder quality and performance settings.
+ *
+ */
 struct cwipc_encoder_params
 {
-    int num_threads;
-    bool do_inter_frame;
-    int gop_size;
-    double exp_factor;
-    int octree_bits;
-    int color_bits;
-    int jpeg_quality;
-    int macroblock_size;
+    int num_threads;        /**< How many concurrent threads to use. */
+    bool do_inter_frame;	/**< (unimplemented) do inter-frame compression */
+    int gop_size;			/**< (unimplemented) spacing of I frames for inter-frame compression */
+    double exp_factor;		/**< Bounding box expansion factor */
+    int octree_bits;		/**< Octree resolution */
+    int color_bits;			/**< Color resolution */
+    int jpeg_quality;		/**< JPEG encoding quality */
+    int macroblock_size;	/**< JPEG macroblock size */
 };
 
+/** \brief Version of cwipc_encoder_params structure.
+ *
+ * The external representation of the encoder parameters may change between versions of
+ * this library. Therefore when you pass the structure to the constructor you should
+ * also pass this version number, to ensure your code is compatible with this version
+ * of the library.
+ */
+#define CWIPC_ENCODER_PARAM_VERSION 0x20190330
+
+#ifdef __cplusplus
 class _CWIPC_CODEC_EXPORT cwipc_codec
 {
 public:
@@ -51,14 +62,202 @@ public:
 protected:
    cwipc_encoder_params param;
 };
+
+/** \brief Pointcloud encoder, abstract C++ interface.
+ *
+ * This interface is provided by pointcloud compressors. The caller
+ * feeds in pointclouds (as `cwipc` objects) and it returns memory blocks
+ * with compressed pointcloud data.
+ */
+class _CWIPC_CODEC_EXPORT cwipc_encoder
+{
+public:
+    virtual ~cwipc_encoder() {}
+
+    /** \brief Deallocate the encoder.
+     *
+     * Because the encoder may be used in a different implementation
+     * language or DLL than where it is implemented we do not count on refcounting
+     * and such. Call this method if you no longer need it.
+     */
+    virtual void free() = 0;
+
+    /** \brief Return true if no more encoded buffers are forthcoming.
+     */
+    virtual bool eof() = 0;
+
+    /** \brief Return true if a data buffer is currently available.
+     * \param wait Set to true if the caller is willing to wait until one is available.
+     * 
+     * If this cwipc_encoder is not multi-threading capable the wait parameter is ignored.
+     * If it is multi-threaded aware and no data is currently available 
+     * it may wait a reasonable amount of time (think: about a second) to see whether
+     * one becomes available.
+     */
+    virtual bool available(bool wait) = 0;
+    
+    /** \brief Encode a pointcloud.
+     *
+     * This call presents the encoder with a new pointcloud to encode.
+     */
+    virtual void feed(cwipc *pc) = 0;
+    
+    /** \brief Return size (in bytes) of compressed data available.
+     *
+     * The caller should first call `available()` to check that compressed
+     * data is available, then `get_encoded_size()` to check how much data is available,
+     * then allocate a buffer of suitable size, then call `copy_data()`.
+     */
+    virtual size_t get_encoded_size() = 0;
+    
+    /** \brief Obtain compressed pointcloud data.
+     *
+     * Copies the data of the most recently compressed pointcloud into a buffer
+     * supplied by the caller.
+     * The compressed data is returned at most once, so after this call
+     * `available()` will return false again until `feed()` has been called.
+     */
+    virtual bool copy_data(void *buffer, size_t bufferSize) = 0;
+};
+
+/** \brief Pointcloud encoder, abstract C++ interface.
+ *
+ * This interface is provided by pointcloud decompressors. The caller
+ * feeds in memory blocks with compressed pointcloud data. The interface
+ * adheres to the `cwipc_source` interface and produces `cwipc` objects on the
+ * output side.
+ */
+class _CWIPC_CODEC_EXPORT cwipc_decoder : public cwipc_source
+{
+public:
+    virtual ~cwipc_decoder() {}
+    virtual void free() = 0;
+    virtual bool eof() = 0;
+    virtual bool available(bool wait) = 0;
+
+    /** \brief Feed compressed data into the decoder.
+	 * \param buffer Pointer to the data buffer containing the compressed pointcloud data.
+	 * \param bufferSize Size of the data buffer.
+     *
+     * Use this call to pass a new compressed pointcloud into the decoder.
+     * After decompression `available()` will return true, and `get()` can be
+     * used to obtain the cwipc pointcloud data.
+     */
+    virtual void feed(void *buffer, size_t bufferSize) = 0;
+    virtual cwipc *get() = 0;
+};
+#else
+typedef struct _cwipc_encoder {
+    int _dummy;
+} cwipc_encoder;
+
+typedef struct _cwipc_decoder {
+    cwipc_source source;
+} cwipc_decoder;
 #endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/** \brief Decompress a single pointcloud.
+ *
+ * (This API should be considered deprecated)
+ * C-compatible API call to decompress a single pointcloud.
+ * \param compFrame A pointer to a buffer with the compressed pointcloud data.
+ * \param len Size of he compFrame buffer (in bytes)
+ * \returns a cwipc object representing the pointcloud.
+ */
 _CWIPC_CODEC_EXPORT cwipc* cwipc_decompress(unsigned char * compFrame, int len);
-    
+
+/** \brief Create pointcloud decompressor.
+ *
+ * \param version Pass in CWIPC_ENCODER_PARAM_VERSION to ensure runtime compatibility.
+ * \param params Pointer to a structure with parameters than govern the encoding process.
+ * \return A cwipc_encoder object.
+ */
+_CWIPC_CODEC_EXPORT cwipc_encoder* cwipc_new_encoder(int version, cwipc_encoder_params* params);
+
+/** \brief Deallocate the encoder (C interface).
+ * \param obj The cwipc_encoder object.
+ *
+ * Because the encoder may be used in a different implementation
+ * language or DLL than where it is implemented we do not count on refcounting
+ * and such. Call this method if you no longer need it.
+ */
+_CWIPC_CODEC_EXPORT void cwipc_encoder_free(cwipc_encoder *obj);
+
+/** \brief Return true if no more encoded buffers are forthcoming (C interface).
+ * \param obj The cwipc_encoder object.
+ */
+_CWIPC_CODEC_EXPORT bool cwipc_encoder_eof(cwipc_encoder *obj);
+
+/** \brief Return true if a data buffer is currently available (C interface).
+ * \param obj The cwipc_encoder object.
+ * \param wait Set to true if the caller is willing to wait until one is available.
+ * 
+ * If this cwipc_encoder is not multi-threading capable the wait parameter is ignored.
+ * If it is multi-threaded aware and no data is currently available 
+ * it may wait a reasonable amount of time (think: about a second) to see whether
+ * one becomes available.
+ */
+_CWIPC_CODEC_EXPORT bool cwipc_encoder_available(cwipc_encoder *obj, bool wait);
+
+/** \brief Encode a pointcloud (C interface).
+ * \param obj The cwipc_encoder object.
+ * \param pc The pointcloud to encode.
+ *
+ * This call presents the encoder with a new pointcloud to encode.
+ */
+_CWIPC_CODEC_EXPORT void cwipc_encoder_feed(cwipc_encoder *obj, cwipc* pc);
+
+/** \brief Return size (in bytes) of compressed data available (C interface).
+ * \param obj The cwipc_encoder object.
+ *
+ * The caller should first call `available()` to check that compressed
+ * data is available, then `get_encoded_size()` to check how much data is available,
+ * then allocate a buffer of suitable size, then call `copy_data()`.
+ */
+_CWIPC_CODEC_EXPORT size_t cwipc_encoder_get_encoded_size(cwipc_encoder *obj);
+
+/** \brief Obtain compressed pointcloud data (C interface).
+ * \param obj The cwipc_encoder object.
+ * \param buffer Pointer to the buffer where the compressed data will be stored.
+ * \param bufferSize Size of buffer (in bytes)
+ *
+ * Copies the data of the most recently compressed pointcloud into a buffer
+ * supplied by the caller.
+ * The compressed data is returned at most once, so after this call
+ * `available()` will return false again until `feed()` has been called.
+ */
+_CWIPC_CODEC_EXPORT bool cwipc_encoder_copy_data(cwipc_encoder *obj, void *buffer, size_t bufferSize);
+
+/** \brief Create pointcloud decompressor.
+ * \returns cwipc_decoder object representing the decompressor.
+ *
+ * After creating the decoder you feed it data using the `feed()` method.
+ * Whenever `available()` returns true a new pointcloud can be obtained with
+ * `get()`.
+ */
+_CWIPC_CODEC_EXPORT cwipc_decoder* cwipc_new_decoder();
+
+/* Methods defined in the cwipc_source superclass, use those */
+#define cwipc_decoder_free(obj) cwipc_source_free(obj)
+#define cwipc_decoder_available(obj, wait) cwipc_source_available(obj, wait)
+#define cwipc_decoder_eof(obj) cwipc_source_eof(obj)
+#define cwipc_decoder_get(obj) cwipc_source_get(obj)
+
+/** \brief Feed compressed data into the decoder (C interface).
+ * \param obj The cwipc_encoder object.
+ * \param buffer Pointer to the data buffer containing the compressed pointcloud data.
+ * \param bufferSize Size of the data buffer.
+ *
+ * Use this call to pass a new compressed pointcloud into the decoder.
+ * After decompression `available()` will return true, and `get()` can be
+ * used to obtain the cwipc pointcloud data.
+ */
+_CWIPC_CODEC_EXPORT void cwipc_decoder_feed(cwipc_decoder *obj, void *buffer, size_t bufferSize);
+
 #ifdef __cplusplus
 }
 #endif
---
+++ python/test_cwipc_codec.py
@@ -0,0 +1,143 @@
+import unittest
+import cwipc
+import cwipc.codec
+import os
+import sys
+import tempfile
+
+if 0:
+    # This code can be used to debug the C++ code in XCode:
+    # - build for XCode with cmake
+    # - build the cwipc_util project
+    # - Fix the pathname for the dylib
+    # - run `python3 test_cwipc_util`
+    # - Attach to python in the XCode debugger
+    # - press return to python3.
+    import cwipc.codec
+    cwipc.codec._cwipc_codec_dll('/Users/jack/src/VRTogether/cwipc_codec/build-xcode/lib/Debug/libcwipc_codec.dylib')
+    print('Type return after attaching in XCode debugger - ')
+    sys.stdin.readline()
+
+#
+# Windows search path is horrible. Work around it for testing with an environment variable
+#
+if 'CWIPC_TEST_DLL' in os.environ:
+	filename = os.environ['CWIPC_TEST_DLL']
+	dllobj = cwipc.codec._cwipc_codec_dll(filename)
+#
+# Find directories for test inputs and outputs
+#
+if 'CWIPC_TEST_DIR' in os.environ:
+    CWIPC_TEST_DIR=os.environ['CWIPC_TEST_DIR']
+else:
+    _thisdir=os.path.dirname(os.path.abspath(__file__))
+    _topdir=os.path.dirname(_thisdir)
+    TEST_FIXTURES_DIR=os.path.join(_topdir, "tests", "fixtures")
+    TEST_OUTPUT_DIR=os.path.join(TEST_FIXTURES_DIR, "output")
+    if not os.access(TEST_OUTPUT_DIR, os.W_OK):
+        TEST_OUTPUT_DIR=tempfile.mkdtemp('cwipc_codec_test')
+PLY_FILENAME=os.path.join(TEST_FIXTURES_DIR, "input", "pcl_frame1.ply")
+COMPRESSED_FILENAME=os.path.join(TEST_FIXTURES_DIR, "compressed", "pcl_frame1.cwicpc")
+
+class TestApi(unittest.TestCase):
+        
+    def test_cwipc_new_encoder(self):
+        """Can we create and free a cwipc_encoder object"""
+        encoder = cwipc.codec.cwipc_new_encoder()
+        self.assertFalse(encoder.eof())
+        self.assertFalse(encoder.available(False))
+        self.assertFalse(encoder.available(True))
+        encoder.free()
+        
+    def test_cwipc_new_decoder(self):
+        """Can we create and free a cwipc_decoder object"""
+        decoder = cwipc.codec.cwipc_new_decoder()
+        self.assertFalse(decoder.eof())
+        self.assertFalse(decoder.available(False))
+        self.assertFalse(decoder.available(True))
+        decoder.free()
+
+    def test_cwipc_encoder_plyfile(self):
+        """Test that we can encode a PLY file and the available flags behaves correct"""
+        pc = cwipc.cwipc_read(PLY_FILENAME, 1234)
+        self._verify_pointcloud(pc)
+        encoder = cwipc.codec.cwipc_new_encoder()
+        self.assertFalse(encoder.available(False))
+        encoder.feed(pc)
+        self.assertTrue(encoder.available(False))
+        data = encoder.get_bytes()
+        self.assertNotEqual(len(data), 0)
+        self.assertFalse(encoder.available(False))
+        encoder.free()
+        pc.free()        
+
+    @unittest.skip('unexpected difference')
+    def test_cwipc_encoder_plyfile_multiple_same(self):
+        """Test that we can encode a ply file multiple times and the results are the same"""
+        pc = cwipc.cwipc_read(PLY_FILENAME, 1234)
+        encoder = cwipc.codec.cwipc_new_encoder()
+        encoder.feed(pc)
+        data_one = encoder.get_bytes()
+        encoder.feed(pc)
+        data_two = encoder.get_bytes()
+        self.assertEqual(data_one, data_two)
+                
+        encoder.free()
+        pc.free()
+
+    def test_cwipc_encoder_plyfile_multiple_different(self):
+        """Test that we can encode a ply file multiple times and the results are the same, and different for different timestamps"""
+        pc = cwipc.cwipc_read(PLY_FILENAME, 1234)
+        encoder = cwipc.codec.cwipc_new_encoder()
+        encoder.feed(pc)
+        data_one = encoder.get_bytes()
+        pc3 = cwipc.cwipc_read(PLY_FILENAME, 4567)
+        encoder.feed(pc3)
+        data_three = encoder.get_bytes()
+        self.assertNotEqual(data_one, data_three)
+        
+        encoder.free()
+        pc.free()
+        pc3.free()
+        
+    def test_cwipc_decode_cwicpc(self):
+        """Test that we can decode a cwicpc compressed pointcloud and get an acceptable cwipc"""
+        with open(COMPRESSED_FILENAME, 'rb') as fp:
+            compdata = fp.read()
+        decoder = cwipc.codec.cwipc_new_decoder()
+        self.assertFalse(decoder.available(False))
+        decoder.feed(compdata)
+        self.assertTrue(decoder.available(False))
+        pc = decoder.get()
+        self.assertFalse(decoder.available(False))
+        self._verify_pointcloud(pc)
+        decoder.free()
+        pc.free()
+        
+    def test_cwipc_codec_roundtrip(self):
+        """Check that we can roundtrip encoder-decoder and get at most as many points back"""
+        pc = cwipc.cwipc_read(PLY_FILENAME, 1234)
+        encoder = cwipc.codec.cwipc_new_encoder()
+        decoder = cwipc.codec.cwipc_new_decoder()
+        encoder.feed(pc)
+        data = encoder.get_bytes()
+        decoder.feed(data)
+        pc2 = decoder.get()
+        self._verify_pointcloud(pc2)
+        points = pc.get_points()
+        points2 = pc2.get_points()
+        self.assertGreaterEqual(len(points), len(points2))
+        encoder.free()
+        decoder.free()
+        pc.free()     
+        pc2.free()   
+
+    def _verify_pointcloud(self, pc):
+        points = pc.get_points()
+        self.assertGreater(len(points), 1)
+        p0 = points[0].x, points[0].y, points[0].z
+        p1 = points[len(points)-1].x, points[len(points)-1].y, points[len(points)-1].z
+        self.assertNotEqual(p0, p1)
+   
+if __name__ == '__main__':
+    unittest.main()
